7e7501b06c35871c116c83f1629670ee839d1bba
Santhosh Srinivasan <santhoshsrinivasan29092001@gmail.com>
2023-11-23 12:52:00 +0530

    Task Completed

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# Title : CALCULATOR
+
+This is the Task for DOM Day-3 (22/11/2023).
+
+This is a Basic Calculator using HTML, CSS, and JavaScript with Dynamic DOM Manipulation. It performs Arithmetic Operations and provide a Seamless User Experience.
---
+++ script.js
@@ -0,0 +1,39 @@
+//Getting and Setting Buttons
+const display = document.querySelector(".display");
+const buttons = document.querySelectorAll("button");
+const specialChars = ["←", "/", "*", "-", "+", "="];
+let output = "";
+
+const calculate = (btnValue) => {
+    if (btnValue === "=" && output !== "") {
+        output = eval(output.replace("%", "/100"));
+    } else if (btnValue === "C") {
+        output = "";
+    } else if (btnValue === "←") {
+        output = output.toString().slice(0, -1);
+    } else {
+        if (output === "" && specialChars.includes(btnValue)) {
+            return;
+        }
+        output += btnValue;
+    }
+    display.value = output;
+};
+
+buttons.forEach((button) => {
+    button.addEventListener("click", (e) => calculate(e.target.dataset.value));
+});
+
+// Add event listener for keyboard events
+document.addEventListener("keydown", (e) => {
+    const key = e.key;
+    // Check if the pressed key corresponds to a button's data-value
+    const button = Array.from(buttons).find((btn) => btn.dataset.value === key);
+    if (button) {
+        button.click(); // Simulate a click on the corresponding button
+    }
+});
+
+
+
+
